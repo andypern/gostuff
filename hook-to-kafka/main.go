bf0935aa7d7c91909222fddf7e5d7641096af81c
Andy Pernsteiner <andypern@gmail.com>
2017-02-01 17:19:13 +0000

    its working

--- hook-to-kafka/main.go
+++ hook-to-kafka/main.go
@@ -6,15 +6,17 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	//"strings"
+	"strings"
 	"os"
-	"encoding/json"
 
 	"github.com/optiopay/kafka"
-	//"github.com/optiopay/kafka/proto"
+	"github.com/optiopay/kafka/proto"
 
 )
-
+//for now
+const (
+	partition =  0
+)
 var (
 	listenPort       = flag.String("listenPort", "8087", "port to listen on")
 	kafkaAddrs     = flag.String("kafkaHost", "localhost:9092 localhost:9093", "space separated host:port pairs for kafka brokers")
@@ -34,39 +36,32 @@ func main() {
 
 
 	//turn our string into a string array
-	//kAddrs := strings.Fields(*kafkaAddrs)
+	kAddrs := strings.Fields(*kafkaAddrs)
 
 	// Here's how we define our kafkaConnection
-	/*kConnect,err := kafka.Dial(kAddrs, kafka.NewBrokerConf("test-client"))
+	kConnect,err := kafka.Dial(kAddrs, kafka.NewBrokerConf("test-client"))
 	if err != nil {
 		fmt.Printf("screwed up on broker: %v\n", err)
 		os.Exit(1)
 	}
-*/
 // create a handler that we can use
-	//kHandler := &kafkaHandler{kafkaClient: kConnect}
+
+	kHandler := &kafkaHandler{kafkaClient: kConnect}
 
 
 	log.Fatal(http.ListenAndServe(":"+*listenPort,
 	 http.HandlerFunc(
 	  func(w http.ResponseWriter, r *http.Request) {
 	    b, _ := ioutil.ReadAll(r.Body)
-		jb, err := json.Marshal(b)
-		if err != nil {
-			fmt.Printf("error serializing event: %v\n", err)
-			os.Exit(1)
-		}
 
-		fmt.Printf("%s\n", jb)
 		//this is where to insert call to kafka func
-
-	    //fmt.Printf("Req: %#v\nBody: %s\n", r, string(b))
+		produceEvent(kHandler.kafkaClient, b)	
+	    //fmt.Printf("Req: %#v\nBody: %s\n", r, b)
+	    fmt.Printf("%s\n", b)
 	    w.WriteHeader(http.StatusOK)
 	})))
 }
 
-
-/*
 func produceEvent(broker kafka.Client, inputEvent []byte) {
     producer := broker.Producer(kafka.NewProducerConf())
     msg := &proto.Message{Value: []byte(inputEvent)}
@@ -76,4 +71,3 @@ func produceEvent(broker kafka.Client, inputEvent []byte) {
 		fmt.Printf("I had an error: %s", err)
     }
 }
-*/
